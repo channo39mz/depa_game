668afd21b11a9769f64fa0dbbcdd69cd2b35ba6e
ItBlockZ <nontapat.boss@gmail.com>
2023-05-25 08:43:45 +0700

    finish shop

--- Assets/Scripts/System/Ingredients.cs
+++ Assets/Scripts/System/Ingredients.cs
@@ -5,13 +5,75 @@ using UnityEngine;
 [System.Serializable]
 public class Ingredients
 {
-    [SerializeField] private LineItem[] items;
+    [SerializeField] private int coins;
+    [SerializeField] private List<LineItem> items;
 
-    public void ShowIngredients()
+    public Ingredients(int coins)
+    {
+        this.coins = coins;
+    }
+
+    public void Add(Item item, int quantity)
+    {
+        items.Add(new LineItem(item, quantity));
+    }
+
+    public bool Trade(Ingredients ingredients)
+    {
+        if (Contain(ingredients))
+        {
+            ingredients.coins -= coins;
+            foreach (LineItem item in items)
+            {
+                ingredients.GetLineItem(item.Item).Quantity -= GetLineItem(item.Item).Quantity;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private bool Contain(Ingredients ingredients)
     {
         foreach (LineItem item in items)
         {
-            Debug.Log(" - " + item);
+            if (ingredients.coins < coins
+                || !ingredients.Contain(item.Item)
+                || ingredients.GetLineItem(item.Item).Quantity < GetLineItem(item.Item).Quantity)
+            {
+                return false;
+            }
         }
+        return true;
+    }
+
+    private bool Contain(Item item)
+    {
+        if (GetLineItem(item) != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private LineItem GetLineItem(Item item)
+    {
+        foreach (LineItem lineItem in items)
+        {
+            if (lineItem.Item.Equals(item))
+            {
+                return lineItem;
+            }
+        }
+        return null;
+    }
+
+    public override string ToString()
+    {
+        string str = items[0].Item.ItemName + "(" + items[0].Quantity + ")";
+        for (int i = 1; i < items.Count; i++)
+        {
+            str += ", " + items[i].Item.ItemName + "(" + items[i].Quantity + ")";
+        }
+        return str;
     }
 }
--- Assets/skip/randomsell.cs
+++ Assets/skip/randomsell.cs
@@ -3,31 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class randomsell : MonoBehaviour
+public class RandomSell : MonoBehaviour
 {
     //public GameObject[] listIsell;
-    public GameObject player;
     public GameObject sellwindow;
     public Button[] sellitem;
     public Sprite[] image;
+    private bool hasCustomer = false;
+    private bool windowActive = false;
+
     public void Start()
     {
-        sellitem[0].image.sprite = image[Random.Range(0, 6)];
-        sellitem[1].image.sprite = image[Random.Range(0, 6)];
-        sellitem[2].image.sprite = image[Random.Range(0, 6)];
+        NewOrder();
     }
-    public void Update()
+
+    private void NewOrder()
     {
-        if (Mathf.Abs(player.transform.position.x - gameObject.transform.position.x) < 5 &&
-            Mathf.Abs(player.transform.position.y - gameObject.transform.position.y) < 5)
+        List<int> random = Choose(image.Length, sellitem.Length);
+        for (int i = 0; i < sellitem.Length; i++)
         {
-            if (Input.GetKey(KeyCode.F))
+            // sellitem[i].image.sprite = image[random[i]];
+        }
+    }
+
+    private List<int> Choose(int n, int c)
+    {
+        List<int> order = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            order.Add(i);
+        }
+        List<int> random = new List<int>();
+        for (int i = n; i >= (n + 1) - c; i--)
+        {
+            int choose = Random.Range(0, i);
+            random.Add(order[choose]);
+            order.RemoveAt(choose);
+        }
+        return random;
+    }
+
+    private void Update() {
+        if (hasCustomer && Input.GetKeyDown(KeyCode.F))
+        {
+            if (!windowActive)
             {
                 sellwindow.SetActive(true);
+                windowActive = true;
                 Debug.Log("IsellU");
             }
-            
+            else
+            {
+                sellwindow.SetActive(false);
+                windowActive = false;
+            }
+        }
+        else if (!hasCustomer)
+        {
+            sellwindow.SetActive(false);
+            windowActive = false;
         }
     }
    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player")
+        {
+            hasCustomer = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player")
+        {
+            hasCustomer = false;
+        }
+    }
 }
