0aae368a96d14c65d253e69a933ca8d06254256f
Tanapat Deesamoot <kong.deesamoot@gmail.com>
2023-05-28 18:11:17 +0700

    new dash system

--- Assets/kong/Script/playerMove.cs
+++ Assets/kong/Script/playerMove.cs
@@ -7,14 +7,22 @@ public class playerMove : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private InputActionReference move;
+    [SerializeField] GameObject trail;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashLength;
     private Rigidbody2D rb;
     private Vector2 movementInput;
     private bool IsPressSpace;
     private Vector2 moveDirection;
     public Animator animator;
+    
+    private float dashCounter;
+    
+    private float activeMoveSpeed;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        activeMoveSpeed = speed;
     }
 
     void Update(){
@@ -27,23 +35,48 @@ public class playerMove : MonoBehaviour
         animator.SetFloat("Horizental",moveDirection.x);
         animator.SetFloat("Vertical",moveDirection.y);
         animator.SetFloat("Speed",moveDirection.sqrMagnitude);
-        rb.velocity = moveDirection * speed;
+        rb.velocity = moveDirection * activeMoveSpeed;
 
-        if(IsPressSpace){
-            Vector2 dashPosition = moveDirection * speed;
-            rb.velocity = new Vector2(dashPosition.x * 10,dashPosition.y * 10);
-            // RaycastHit2D rayCastHit = Physics2D.Raycast(transform.position,moveDirection,10,);
-            IsPressSpace = false;
+        // if(Input.GetKeyDown(KeyCode.Space)){
+        //     if(dashCounter <= 0){
+        //         activeMoveSpeed = dashSpeed;
+        //         dashCounter = dashLength;
+        //     }
+        //     // Vector2 dashPosition = moveDirection * speed;
+        //     // trail.enabled = true;
+        //     // rb.velocity = new Vector2(dashPosition.x * 10,dashPosition.y * 10);
+        //     // StartCoroutine(DashDelay(0.1f));
+        //     // // aycastHit2D rayCastHit = Physics2D.Raycast(transform.position,moveDirection,10,);
+        //     // IsPressSpace = false;
+        // }
+
+        if(dashCounter > 0){
+            trail.SetActive(true);
+            dashCounter -= Time.deltaTime;
+            if(dashCounter <= 0){
+                trail.SetActive(false);
+                activeMoveSpeed = speed;
+            }
         }
     }
     public void dash(InputAction.CallbackContext context){
         if(context.phase == InputActionPhase.Performed){
-            IsPressSpace = true;
+            if(dashCounter <= 0){
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
         }
     }
 
     public void SetSpeed(float speed)
     {
-        this.speed = speed;
+        activeMoveSpeed = speed;
+        // dashSpeed = speed * 1.5f;
     }
+
+    // private IEnumerator DashDelay(float time){
+    //     yield return new WaitForSeconds(time);
+    //     trail.enabled = false;
+    //     dashCD = false;
+    // }
 }
