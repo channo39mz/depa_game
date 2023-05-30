6c4aa766b2273fd3bddb224dbfa9fc934164552e
Tanapat Deesamoot <kong.deesamoot@gmail.com>
2023-05-30 22:52:41 +0700

    skill and icon fix

--- Assets/kong/Script/playerMove.cs
+++ Assets/kong/Script/playerMove.cs
@@ -10,45 +10,25 @@ public class playerMove : MonoBehaviour
     [SerializeField] GameObject trail;
     [SerializeField] float dashSpeed;
     [SerializeField] float dashLength;
-    private Rigidbody2D rb;
-    private Vector2 movementInput;
-    private bool IsPressSpace;
-    private Vector2 moveDirection;
-    public Animator animator;
-    
     private float dashCounter;
-    
     private float activeMoveSpeed;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         activeMoveSpeed = speed;
     }
 
     void Update(){
-        movementInput = move.action.ReadValue<Vector2>();
+        // movementInput = move.action.ReadValue<Vector2>();
     }
 
     void FixedUpdate()
     {
-        moveDirection = new Vector2(movementInput.x, movementInput.y).normalized;
-        animator.SetFloat("Horizental",moveDirection.x);
-        animator.SetFloat("Vertical",moveDirection.y);
-        animator.SetFloat("Speed",moveDirection.sqrMagnitude);
-        rb.velocity = moveDirection * activeMoveSpeed;
-
-        // if(Input.GetKeyDown(KeyCode.Space)){
-        //     if(dashCounter <= 0){
-        //         activeMoveSpeed = dashSpeed;
-        //         dashCounter = dashLength;
-        //     }
-        //     // Vector2 dashPosition = moveDirection * speed;
-        //     // trail.enabled = true;
-        //     // rb.velocity = new Vector2(dashPosition.x * 10,dashPosition.y * 10);
-        //     // StartCoroutine(DashDelay(0.1f));
-        //     // // aycastHit2D rayCastHit = Physics2D.Raycast(transform.position,moveDirection,10,);
-        //     // IsPressSpace = false;
-        // }
+        Vector2 movementInput = move.action.ReadValue<Vector2>();
+        Vector2 moveDirection = new Vector2(movementInput.x, movementInput.y).normalized;
+        this.GetComponent<Animator>().SetFloat("Horizental",moveDirection.x);
+        this.GetComponent<Animator>().SetFloat("Vertical",moveDirection.y);
+        this.GetComponent<Animator>().SetFloat("Speed",moveDirection.sqrMagnitude);
+        this.GetComponent<Rigidbody2D>().velocity = moveDirection * activeMoveSpeed;
 
         if(dashCounter > 0){
             trail.SetActive(true);
